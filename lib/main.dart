9621148d7d755e8c956abcf7dc09ecd5b6f8cf5c
SaraZuhair <zuhairsara15@gmail.com>
2021-11-20 21:05:01 +0300

    we made the appbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,223 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+// ignore: camel_case_types
+class exercise extends StatefulWidget {
+  const exercise({Key? key}) : super(key: key);
+
+  @override
+  _exerciseState createState() => _exerciseState();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
+class _exerciseState extends State<exercise> {
+  // ignore: non_constant_identifier_names
+  bool SizeisChecked = true;
+  bool ColorisChecked = true;
+  double red = 0.0;
+  double blue = 0.0;
+  double green = 0.0;
+  double sz = 200.0;
+  double s = 100;
+  double small = 50;
+  double med = 300;
+  double large = 500;
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text("My Icon"),
+        backgroundColor: Colors.brown,
+        actions: [
+          Container(
+            height: 30,
+            width: 30,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              border: Border.all(
+                color: Colors.white,
+                width: 2,
+              ),
+            ),
+            child: Center(
+              child: IconButton(
+                padding: EdgeInsets.only(
+                  right: 5,
+                ),
+                onPressed: () {
+                  if (SizeisChecked) {
+                    setState(() {
+                      if (sz - small == 0 || sz - small < 0) {
+                        sz = 1;
+                      } else {
+                        sz = sz - small;
+                      }
+                    });
+                  }
+                },
+                icon: const Icon(Icons.remove),
+              ),
+            ),
+          ),
+          const SizedBox(
+            width: 7,
+          ),
+          GestureDetector(
+            onTap: () {
+              if (SizeisChecked) {
+                setState(() {
+                  sz = s;
+                });
+              }
+            },
+            child: Container(
+              height: 30,
+              width: 30,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                border: Border.all(
+                  color: Colors.white,
+                  width: 2,
+                ),
+              ),
+              child: const Center(
+                child: Text("S",
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 12)),
+              ),
+            ),
+          ),
+          const SizedBox(
+            width: 7,
+          ),
+          GestureDetector(
+            onTap: () {
+              if (SizeisChecked) {
+                setState(() {
+                  sz = med;
+                });
+              }
+            },
+            child: Container(
+              height: 30,
+              width: 30,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                border: Border.all(
+                  color: Colors.white,
+                  width: 2,
+                ),
+              ),
+              child: const Center(
+                child: Text("M",
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 12)),
+              ),
+            ),
+          ),
+          const SizedBox(
+            width: 7,
+          ),
+          GestureDetector(
+            onTap: () {
+              if (SizeisChecked) {
+                setState(() {
+                  sz = large;
+                });
+              }
+            },
+            child: Container(
+              height: 30,
+              width: 30,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                border: Border.all(
+                  color: Colors.white,
+                  width: 2,
+                ),
+              ),
+              child: const Center(
+                child: Text("L",
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 12)),
+              ),
+            ),
+          ),
+          const SizedBox(
+            width: 7,
+          ),
+          Container(
+            height: 30,
+            width: 30,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              border: Border.all(
+                color: Colors.white,
+                width: 2,
+              ),
+            ),
+            child: Center(
+              child: IconButton(
+                  padding: EdgeInsets.only(
+                    right: 10,
+                  ),
+                  onPressed: () {
+                    if (SizeisChecked) {
+                      setState(() {
+                        if (sz + small > 1500) {
+                          sz = 1500;
+                        } else {
+                          sz = sz + small;
+                        }
+                      });
+                    }
+                  },
+                  icon: const Icon(Icons.add)),
+            ),
+          ),
+          const SizedBox(
+            width: 7,
+          ),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            ListTile(
+              leading: const Text("Allow Resize"),
+              trailing: Checkbox(
+                value: SizeisChecked,
+                onChanged: (value) {
+                  setState(() {
+                    SizeisChecked = value!;
+                  });
+                },
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            ListTile(
+              leading: const Text("Allow change primer color"),
+              trailing: Checkbox(
+                value: ColorisChecked,
+                onChanged: (value) {
+                  setState(() {
+                    ColorisChecked = value!;
+                  });
+                },
+              ),
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+void main() {
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'My app', // used by the OS task switcher
+      home: SafeArea(
+        child: exercise(),
+      ),
+    ),
+  );
 }
